30e85f9299027cc46b7b5c5c592974281636bd19
Mhar Andal <mharandal96@gmail.com>
2018-04-05 18:44:51 -0500

    :white_check_mark:Adds initial test setup and deployment test case:white_check_mark:

---
+++ test/Lottery.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  lottery.setProvider(provider);
+});
+
+describe('Lottery', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
+});
