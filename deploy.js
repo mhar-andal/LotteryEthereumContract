8f469029fbfdc0aae0e51e458c2ff84288552a01
Mhar Andal <mharandal96@gmail.com>
2018-04-05 18:33:26 -0500

    :wrench:Remove initial argument used for the Inbox contract:wrench:

--- deploy.js
+++ deploy.js
@@ -16,7 +16,7 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const contractInstance = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
 
   console.log('Contract deployed to', contractInstance.options.address);
