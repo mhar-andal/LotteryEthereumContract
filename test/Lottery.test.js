3fc24e4feb5d8c900afb2cfb440e82c1b4993c9c
Mhar Andal <mharandal96@gmail.com>
2018-04-05 23:48:07 -0500

    :white_check_mark:Adds test case for sending money to the winner and reseting the players:white_check_mark:

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -73,4 +73,38 @@ describe('Lottery', () => {
       assert(err);
     }
   });
+
+  it('only manager can call pickWinner', async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('sends money to the winner and resets the players array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether'),
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    const difference = finalBalance - initialBalance;
+
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
+    assert.equal(0, players.length);
+  });
 });
